9115014f94ea08dfcee0762203163b32b568239f
Miguel <miguelriostangarife@gmail.com>
2025-08-16 11:50:53 -0500

    feat:Create folders structure and Constants file

---
+++ gitsandf/lib/utils/Constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+//TYPHOGRAPHY
+
+const int smallest = 10;
+const int small = 13;
+const int regular = 16;
+const int big = 20;
+const int bigger = 25;
+const int biggest = 31;
+const int large = 39;
+const int larger = 49;
+const int largest = 61;
+
+//COLORS
+
+const primaryColor = Color(0xFFFFCA00);
+const secondaryColor = Color(0xFF008B50);
+const tertiaryColor = Color(0xFF005D35);
+const lighterGrayColor = Color(0xFFEEEEEE);
+const neutralGrayColor = Color(0xFF766F6F);
+const strongerGrayColor = Color(0xFF555555);
+const lighterBlackColor = Color(0xFF4E4E4E);
+const neutralBlackColor = Color(0xFF333333);
+const strongerBlackColor = Color(0xFF000000);
+const whiteColor = Color(0xFFFFFFFF);
+const errorColor = Color(0xFFFF0000);
+const linkColor = Color(0xFF034AF3);
